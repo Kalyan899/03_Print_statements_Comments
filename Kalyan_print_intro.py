aa2114f3721a33e639c27e946d4013b580a7f446
Kalyan899 <105037610+Kalyan899@users.noreply.github.com>
2022-05-06 12:24:17 +0530

    Add files via upload

---
+++ Kalyan_print_intro.py
@@ -0,0 +1,64 @@
+# Name: Kalyan
+# Date: 04/05/2022
+# File Description: print statement
+
+# To print Hello Python Programming Language
+
+# print('Hello Python Programming Language')
+# print("Hello Python Programming Language")
+# print('''Hello Python Programming Language''')
+# print("""Hello Python Programming Language""")
+
+# To print Hello Kal'yan
+
+# print("Hello Kal'yan")
+# print('''Hello Kal'yan''')
+# print("""Hello Kal'yan""")
+# print('Hello Kal\'yan')
+
+# To print Hello Kal"ya"n
+
+# print("Hello Kal\"ya\"n")
+# print('Hello Kal"ya"')
+# print('''Hello Kal"ya"n''')
+# print("""Hello Kal"ya"n""")
+
+# To print Hello(Kalyan how are you)
+
+# print('Hello(Kalyan how are you)')
+# print("Hello(Kalyan how are you)")
+# print('''Hello(Kalyan how are you)''')
+print("""Hello(Kalyan how are you)""")
+
+
+'''Hi
+Kalyan
+How are you?'''
+
+
+
+
+
+
+
+
+
+
+# To make single line Comment
+# ctrl+/
+
+# To make double line Comment without print statement @single quote and double quote
+'''
+Hello Python
+Programming
+Language
+'''
+
+
+# To work with multipul linese
+# print('''Hello
+# Python Programming
+# Language''')
+# print("""Hello
+# Python""")
+
